196080109ea5247d8f1208fc4974320afcb83486
sakibullah2006 <sakibullah.dev@gmail.com>
2026-01-04 07:36:04 +0600

    feat: add @alpinejs/anchor dependency and integrate with Alpine.js plugins; enhance mobile menu functionality

--- src/js/app.js
+++ src/js/app.js
@@ -4,10 +4,16 @@ import collapse from "@alpinejs/collapse";
 import focus from "@alpinejs/focus";
 import slider from "./slider.js";
 import collapsible from "./header/collapsible.js";
+import anchor from "@alpinejs/anchor";
 
 window.Alpine = Alpine;
+
+// plugins
 Alpine.plugin(collapse);
 Alpine.plugin(focus);
+Alpine.plugin(anchor);
+
+// data
 Alpine.data("slider", slider);
 Alpine.data("collapsible", collapsible);
 Alpine.start();
